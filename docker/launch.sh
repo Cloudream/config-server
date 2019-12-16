349d10a702a52479c223f2cb41f5a9978a7bdabc
cloudream <ubuntu_vip@sina.cn>
2019-12-16 14:12:18 +0800

    modify docker port

--- docker/launch.sh
+++ docker/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 REGISTRY_URL=192.168.199.135:2375
 WORK_DIR=/root/work_build
-PROJECT_NAME=config-server
+PROJECT_NAME=eureka-server
 if [ ! -e ${WORK_DIR}/${PROJECT_NAME} ] && [ ! -d ${WORK_DIR}/${PROJECT_NAME} ]; then
 mkdir -p ${WORK_DIR}/${PROJECT_NAME}
 echo "Create Dir: ${WORK_DIR}/${PROJECT_NAME}"
@@ -10,8 +10,8 @@ if [ -e ${WORK_DIR}/${PROJECT_NAME}/Dockerfile ]; then
 rm -rf ${WORK_DIR}/${PROJECT_NAME}/Dockerfile
 echo "Remove File: ${WORK_DIR}/${PROJECT_NAME}/Dockerfile"
 fi
-cp /root/.jenkins/workspace/${PROJECT_NAME}/docker/Dockerfile ${WORK_DIR}/${PROJECT_NAME}/
-cp /root/.jenkins/workspace/${PROJECT_NAME}/build/libs/*.jar ${WORK_DIR}/${PROJECT_NAME}/
+cp /root/.jenkins/workspace/eureka-server/docker/Dockerfile ${WORK_DIR}/${PROJECT_NAME}/
+cp /root/.jenkins/workspace/eureka-server/build/libs/*.jar ${WORK_DIR}/${PROJECT_NAME}/
 cd ${WORK_DIR}/${PROJECT_NAME}/
 docker build -t ${REGISTRY_URL}/eshop-detail/${PROJECT_NAME} .
 docker push ${REGISTRY_URL}/eshop-detail/${PROJECT_NAME}
@@ -19,4 +19,4 @@ if docker ps -a | grep ${PROJECT_NAME}; then
 docker rm -f ${PROJECT_NAME}
 echo "Remove Docker Container: ${PROJECT_NAME}"
 fi
-docker run -d -p 8761:8761 --name ${PROJECT_NAME} ${REGISTRY_URL}/eshop-detail/${PROJECT_NAME}
+docker run -d -p 8766:8766 --name ${PROJECT_NAME} ${REGISTRY_URL}/eshop-detail/${PROJECT_NAME}
